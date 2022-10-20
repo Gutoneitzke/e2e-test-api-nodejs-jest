bf968e1233eca7f765c232e77950b24c04c0c8e7
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-10-20 13:22:26 -0300

    feat: new tests to GET and POST methods

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -5,8 +5,31 @@ import {
     describe
 } from '@jest/globals'
 
+import supertest from 'supertest'
+import Server from '../../src/server.js'
+
 describe('API E2E Test Suite', () => {
     test.todo('GET / - should return an array')
+    test('GET / - should return an array', async () => {
+        const response = await supertest(Server)
+        .get('/')
+
+        const data = JSON.parse(response.text);
+        expect(data).toBeInstanceOf(Array)
+
+        console.log('text',data)
+    })
     test.todo('POST / - should save and item and return ok')
+    test('POST / - should save and item and return ok', async () => {
+        const response = await supertest(Server)
+        .post('/')
+        .send({
+            nome: "Gustavo Neitzke",
+            age: 20
+        })
+
+        const expectResponse = JSON.stringify({ "ok": 1 });
+        expect(response.text).toStrictEqual(expectResponse)
+    })
     test.todo('DELETE / - should return an array')
 })
