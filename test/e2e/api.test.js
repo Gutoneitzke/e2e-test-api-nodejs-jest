a951fdd1dc6695ec2f6dbb3c32b705bd7b645379
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-10-20 13:52:31 -0300

    feat: new tests

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -9,11 +9,13 @@ import supertest from 'supertest'
 import Server from '../../src/server.js'
 
 describe('API E2E Test Suite', () => {
-    test.todo('GET / - should return an array')
+    test.todo('GET / - should return an array with length 0')
     test.todo('POST / - should save and item and return ok')
+    test.todo('GET / - should return an array with length 1')
     test.todo('DELETE / - should return an array')
+    test.todo('GET / - should return an array with length 0')
 
-    test('GET / - should return an array', async () => {
+    test('GET / - should return an array with length 0', async () => {
         const response = await supertest(Server)
         .get('/')
 
@@ -36,11 +38,33 @@ describe('API E2E Test Suite', () => {
         expect(JSON.parse(response.text)).toStrictEqual(expectResponse)
     })
 
+    test('GET / - should return an array with length 1', async () => {
+        const response = await supertest(Server)
+        .get('/')
+
+        const data = JSON.parse(response.text);
+        expect(data).toBeInstanceOf(Array)
+        expect(data.length).toEqual(1)
+
+        console.log('text',data)
+    })
+
     test('DELETE / - should return an array', async () => {
         const response = await supertest(Server)
         .delete('/')
 
         const expectResponse = { ok: 1 }
         expect(JSON.parse(response.text)).toStrictEqual(expectResponse)
+    })
+
+    test('GET / - should return an array with length 0', async () => {
+        const response = await supertest(Server)
+        .get('/')
+
+        const data = JSON.parse(response.text);
+        expect(data).toBeInstanceOf(Array)
+        expect(data.length).toEqual(0)
+
+        console.log('text',data)
     })
 })
