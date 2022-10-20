dcccbed5246d05c3c9a54e301368a78a8f25255e
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-10-20 12:45:28 -0300

    returns to endpoints

--- src/server.js
+++ src/server.js
@@ -1,7 +1,30 @@
 import { createServer } from 'http'
+import { once } from 'event'
+const Database = new Map()
+
+function respondJson(data, response){
+    return response(JSON.stringify(data))
+}
 
 async function handler(request, response) {
-    response.end('Hello world')
+    const { method } = request
+
+    if(method === 'GET')
+    {
+        return respondJson([...Database.values()], response);
+    }
+
+    if(method === 'POST')
+    {
+        const body = JSON.parse(await once(request, 'data'))
+        console.log('recebido',body)
+        return respondJson({ ok: 1 });
+    }
+
+    if(method === 'DELETE')
+    {
+        return respondJson({ ok: 1 });
+    }
 }
 
 export default createServer(handler)
